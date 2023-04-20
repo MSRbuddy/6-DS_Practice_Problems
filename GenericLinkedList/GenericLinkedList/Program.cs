980993a1d784486bffba40f0fb4d9278cd150215
MSRbuddy <meghanadasari222@gmail.com>
2023-04-20 15:29:59 +0530

    Added : Generic class custom LinkedList!

---
+++ GenericLinkedList/GenericLinkedList/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace GenericLinkedList
+{
+    // generic linked list node
+    public class GenericNode<T>
+    {
+        public T data;
+        public GenericNode<T> nextNode = null;
+
+        public GenericNode(T data)
+        {
+            this.data = data;
+        }
+    }
+
+    // generic linked list
+    public class GenericLinkedList<T>
+    {
+        private GenericNode<T> head = null;
+
+        public void Add(T newListItem)
+        {
+            if (head == null)
+            {
+                head = new GenericNode<T>(newListItem);
+            }
+            else
+            {
+                GenericNode<T> curr = head;
+                while (curr.nextNode != null)
+                {
+                    curr = curr.nextNode;
+                }
+                curr.nextNode = new GenericNode<T>(newListItem);
+            }
+        }
+
+        public void DisplayNodes()
+        {
+            GenericNode<T> curr = head;
+            while (curr != null)
+            {
+                Console.WriteLine(curr.data);
+                curr = curr.nextNode;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**********Generic Class Custom Linked-List**********");
+            Console.WriteLine("------------------------------------------------------");
+
+            Console.WriteLine("\nElements in the Linked-List:");
+            GenericLinkedList<Object> genericll = new GenericLinkedList<Object>();
+            genericll.Add(525);
+            genericll.Add("Meghana");
+            genericll.Add('D');
+            genericll.Add(5.1f);
+            genericll.Add(9876543210);
+            genericll.Add("SoftwareEngineer");
+            genericll.Add(true);
+            genericll.DisplayNodes();
+        }
+    }
+}
